d2d14466291b0c21bd25cb9d853d019f5538eddd
CristopherBv <a21201372@alumnos.uady.mx>
2026-01-24 21:53:35 -0600

    se añadieron los metodos para cargar el archivo, validarlo, agregar calificaciones y exportar el archivo :)

--- src/Main/Java/org.cris.AdaDos/tareaUno/tareaUno.java
+++ src/Main/Java/org.cris.AdaDos/tareaUno/tareaUno.java
@@ -17,7 +17,6 @@ public class tareaUno {
     private static class Alumno {
         String matricula, primerApellido, segundoApellido, nombres;
         Integer calificacion;
-
         public Alumno(String matricula, String primerApellido, String segundoApellido, String nombres) {
             this.matricula = matricula;
             this.primerApellido = primerApellido;
@@ -25,14 +24,79 @@ public class tareaUno {
             this.nombres = nombres;
             this.calificacion = null;
         }
-
         public String getNombreAlumno() {
             return nombres + " " + primerApellido + " " + segundoApellido;
         }
-
         public String getMatricula() {
             return matricula;
         }
     }
 
+    // /***************MIRANDA MODIFICA LA RUTA DONDE LO VAYAS A ALMACENAR*******************************************************/
+    private String rutaEntrada = "/home/cristopher/Cuarto_Semestre/DisenoDeSoftware/ADA2/ArchivosCSV/tabla_alumnos.csv";
+    private String rutaSalida = "/home/cristopher/Cuarto_Semestre/DisenoDeSoftware/ADA2/ArchivosCSV/calificaciones_alumnos.csv";
+    private List<Alumno> alumnos = new ArrayList<>();
+    private Scanner scanner = new Scanner(System.in);
+
+    public void generacionCalificaciones() {
+        //Seleccionar archivo
+        System.out.println("Ruta actual: " + rutaEntrada);
+        System.out.print("Presione Enter para usarla o ingrese una nueva ruta:");
+        String nuevaRuta = scanner.nextLine();
+        if (!nuevaRuta.isEmpty()){
+            rutaEntrada = nuevaRuta;
+        }
+        //*************+VALIDADOR ARCHIVO***********
+        if (!rutaEntrada.endsWith(".csv")) {
+            System.err.println("Error: El archivo debe ser .csv");
+            return;
+        }
+        /******NO BORRAR!!!!!!!!*******/
+        if (!cargarDatos()){
+            return;
+        }
+
+        System.out.println("CAPTURA DE CALIFICACIONES (Disenio de Sofatware)");
+        for (Alumno alumno : alumnos) {
+            System.out.print("Ingresa la calificación de " + alumno.getNombreAlumno() + "("+ alumno.getMatricula()+"): ");
+            alumno.calificacion = Integer.parseInt(scanner.nextLine());
+        }
+
+        System.out.print("\n¿Desea generar el archivo csv con las calificaciones (S/N)? ");
+        if (scanner.nextLine().equalsIgnoreCase("S")) {
+            generarArchivoSalida();
+        } else {
+            System.out.println("Proceso cancelado.");
+        }
+    }
+    private boolean cargarDatos() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(rutaEntrada), StandardCharsets.UTF_8))) {
+            br.readLine();
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                String[] dato = linea.split(",");
+                if (dato.length != 4) {
+                    System.err.println("El archivo NO tiene las 4 columnas requeridas.");
+                    return false;
+                }
+                alumnos.add(new Alumno(dato[0], dato[1], dato[2], dato[3]));
+            }
+            return true;
+        } catch (Exception e) {
+            System.err.println("Error al leer el archivo:" + e.getMessage());
+            return false;
+        }
+    }
+
+    private void generarArchivoSalida() {
+        try (PrintWriter pw = new PrintWriter(new OutputStreamWriter(new FileOutputStream(rutaSalida), StandardCharsets.UTF_8))) {
+            pw.println("Matricula,Nombre Asignatura,Calificacion");
+            for (Alumno a : alumnos) {
+                pw.println(a.getMatricula() + ",Disenio de Software," + a.calificacion);
+            }
+            System.out.println("Archivo generado en: " + rutaSalida);
+        } catch (IOException e) {
+            System.err.println("Error al escribir el archivo.");
+        }
+    }
 }
