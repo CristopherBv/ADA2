30e01c1c62dfd2fce5cf0c8dcfd34765b1d81de7
CristopherBv <a21201372@alumnos.uady.mx>
2026-01-24 19:39:41 -0600

    Initial commit ggs

---
+++ src/Main/Java/org.cris.AdaDos/tareaUno/tareaUno.java
@@ -0,0 +1,12 @@
+package org.cris.AdaDos.tareaUno;
+/*
+1. Entrada: un archivo csv con lista de alumnos y 4 columnas
+2. El programa permitirá capturar las calificaciones de "Diseño de software"
+de todos los estudiantes de la lista (del 1 al 100 puros enteros)
+3. Capturadas todas las calificaciones el usuario tendrá una opción para
+generar un archivo CSV con 3 columnas: matricula, nombre asignatura y calificación.
+4. No se podrá generar el archivo de calificaciones, punto 3, si existe uno o
+más alumnos a los que no se les haya capturado calificación. (try catch?)
+*/
+public class tareaUno {
+}
